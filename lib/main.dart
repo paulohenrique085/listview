17117baaa7caf1ad0331ebf042e5ed43977ee417
paulohenrique085 <paulohnq85@outlook.com>
2021-04-26 22:20:49 -0300

    introdução appbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List view"),
+      ),
+    );
   }
 }

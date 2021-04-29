53f34f7ac1e2231c10b4d6129507c297afddf09d
paulohenrique085 <paulohnq85@outlook.com>
2021-04-28 23:17:23 -0300

    create list

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //criando uma lista estática
+  List _itens = [];
+
+  void _recuperarItens() {
+    _itens = [];
+    //ultilizando for para realizar a montagem dos itens
+    for (int i = 0; i <= 10; i++) {
+      //para cada interação será criado um novo Map
+      Map<String, dynamic> item = Map();
+      item["titulo"] = "Titulo ${i} lorem ipsum";
+      item["descrição"] = "descrição ${i} lorem ipsum";
+      //adicionando o item a lista de itens
+      _itens.add(item);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    //antes de chamar o build carregar os itens
+    _recuperarItens();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("List view"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("List view"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          //criando uma lista
+          child: ListView.builder(
+              //de acordo com o tamanho da list(lenght)
+              itemCount: _itens.length,
+              itemBuilder: (context, indice) {
+                return ListTile(
+                  title: Text(_itens[indice]["titulo"]),
+                  subtitle: Text(_itens[indice]["descrição"]),
+                );
+              }),
+        ));
   }
 }
